b8c6bc3ada2d83b7c1c45884c8c21dbb1aee5d99
wja123 <w.j.alag@gmail.com>
2017-09-26 01:02:16 -0400

    Got the LED to work with a timer

--- main.py
+++ main.py
@@ -19,10 +19,18 @@ GPIO.setup(17, GPIO.OUT)
 GPIO.setup(27, GPIO.OUT)
 GPIO.setup(22, GPIO.OUT)
 
-def ledOn(pin):
-    GPIO.output(pin, True)
+def ledOn():
+    GPIO.output(4, True)
+    print 'ledOn: 4'
+    toff = Timer(5, ledOff)
+    toff.start()
 
+def ledOff():
+    print 'ledOff: 4'
+    GPIO.output(4, False)
 
+t = Timer(10, ledOn)
+t.start()
 #GPIO.output(4, True)
 #GPIO.output(18, True)
 #GPIO.output(17, True)
@@ -30,11 +38,11 @@ def ledOn(pin):
 #GPIO.output(22, True)
 
 
-GPIO.output(4, False)
-GPIO.output(18, False)
-GPIO.output(17, False)
-GPIO.output(27, False)
-GPIO.output(22, False)
+#GPIO.output(4, False)
+#GPIO.output(18, False)
+#GPIO.output(17, False)
+#GPIO.output(27, False)
+#GPIO.output(22, False)
 
 
 
