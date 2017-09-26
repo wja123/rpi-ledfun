75d8779fd727c5596519ecb13db82b8f7d474109
wja123 <w.j.alag@gmail.com>
2017-09-26 00:52:03 -0400

    Started implementing the Timer

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import RPi.GPIO as GPIO
+from time import sleep
+from threading import Timer
 
 try:
     import RPi.GPIO as GPIO
@@ -9,17 +11,23 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
 mode = GPIO.getmode()
+pinArr = [4, 18, 17, 27, 22]
+
 GPIO.setup(4, GPIO.OUT)
 GPIO.setup(18, GPIO.OUT)
 GPIO.setup(17, GPIO.OUT)
 GPIO.setup(27, GPIO.OUT)
 GPIO.setup(22, GPIO.OUT)
 
-# GPIO.output(4, True)
-# GPIO.output(18, True)
-# GPIO.output(17, True)
-# GPIO.output(27, True)
-# GPIO.output(22, True)
+def ledOn(pin):
+    GPIO.output(pin, True)
+
+
+#GPIO.output(4, True)
+#GPIO.output(18, True)
+#GPIO.output(17, True)
+#GPIO.output(27, True)
+#GPIO.output(22, True)
 
 
 GPIO.output(4, False)
